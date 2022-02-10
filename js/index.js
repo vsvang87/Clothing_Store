27ad0f2520f99d8387aa5f4f158f9fee5092bcfc
Visay Vang <vsvang@gmail.com>
2022-02-09 21:58:04 -0600

    February9 pt2 update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-const toggleMenu = document.getElementsByClassName("toggle-menu")[0];
-const navLinks = document.getElementsByClassName("navbar")[0];
+const bar = document.getElementById("bar");
+const navLinks = document.getElementById("navbar");
+const close = document.getElementById("close");
 
-toggleMenu.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
+//click to open hamburger menu
+if (bar) {
+  bar.addEventListener("click", () => {
+    navLinks.classList.add("active");
+  });
+}
+//click to close hamburger menu
+if (close) {
+  close.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+}
