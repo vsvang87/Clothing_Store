ac6c5ed649fa12dc8dc9e1f2fc755cacccc999a9
Visay Vang <vsvang@gmail.com>
2022-02-10 14:11:01 -0600

    February 10, 1st update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+//Toggle Hamburger Menu Selectors
 const bar = document.getElementById("bar");
 const navLinks = document.getElementById("navbar");
 const close = document.getElementById("close");
+//selecting small shirt image
+const mainImg = document.getElementById("MainImg");
+const smallImg = document.getElementsByClassName("small-img");
 
 //click to open hamburger menu
 if (bar) {
@@ -14,3 +18,16 @@ if (close) {
     navLinks.classList.remove("active");
   });
 }
+//change small shir img to main when clicked
+smallImg[0].addEventListener("click", () => {
+  mainImg.src = smallImg[0].src;
+});
+smallImg[1].addEventListener("click", () => {
+  mainImg.src = smallImg[1].src;
+});
+smallImg[2].addEventListener("click", () => {
+  mainImg.src = smallImg[2].src;
+});
+smallImg[3].addEventListener("click", () => {
+  mainImg.src = smallImg[3].src;
+});
